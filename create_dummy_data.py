d3f9bc29e445dcd5bbe16b8f1b4e28e04919e817
harupy <17039389+harupy@users.noreply.github.com>
2020-08-09 11:25:33 +0900

    Use model registry

--- create_dummy_data.py
+++ create_dummy_data.py
@@ -1,6 +1,12 @@
 import random
 
 import mlflow
+import mlflow.pyfunc
+
+
+class DummyModel(mlflow.pyfunc.PythonModel):
+    def predict(self, context, model_input):
+        return 0
 
 
 def main():
@@ -8,6 +14,10 @@ def main():
         with mlflow.start_run():
             mlflow.log_param("p", random.random())
             mlflow.log_param("m", random.random())
+
+            mlflow.pyfunc.log_model(
+                "model", python_model=DummyModel(), registered_model_name="pyfunc"
+            )
 
 
 if __name__ == "__main__":
